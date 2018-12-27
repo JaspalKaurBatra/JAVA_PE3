65ca11ba4780049f304b8b497280dfb06c7dd4e7
jasbatr1 <jaspalkaurbatra95@gmail.com>
2018-12-27 16:38:03 +0530

    test case for 8th question

---
+++ src/test/java/com/stackroute/ExceptionTypesTest.java
@@ -0,0 +1,49 @@
+package com.stackroute;
+
+/*
+Write a program that will generate exceptions of type
+ NegativeArraySizeException,
+ IndexOutOfBoundsException,
+ NullPointerException.
+Record the catching of each exception by displaying the message stored in the exception object.
+*/
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExceptionTypesTest {
+
+    private ExceptionTypes exc;
+
+    @Before
+    public void setUp() throws Exception {
+        exc = new ExceptionTypes();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        exc = null;
+    }
+
+    @Test
+    public void NegativeArraySizeExceptionTest(){
+        assertEquals("class java.lang.NegativeArraySizeException", exc.ArrayCreation());
+    }
+
+    @Test
+    public void IndexOutOfBoundsException(){
+        assertEquals("class java.lang.ArrayIndexOutOfBoundsException", exc.ArrayAccess());
+    }
+
+    @Test
+    public void NullPointerException(){
+
+         assertEquals("class java.lang.NullPointerException", exc.StringAccess());
+    }
+
+
+}
